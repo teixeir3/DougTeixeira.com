21dbe3cc710fa125ccc7f7e6304e0c6a958dbf19
Douglas Teixeira <teixeir3@gmail.com>
2015-08-07 11:38:11 -0400

    Add bug fix for error in session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-DougTeixeira::Application.config.session_store :cookie_store, key: '_dougteixeira_session', domain: 'dougteixeira.com'
-
-
+DougTeixeira::Application.config.session_store :cookie_store, key: '_dougteixeira_session'
