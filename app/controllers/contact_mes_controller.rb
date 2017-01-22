cfe220e04bc4144fac451bc97b4f210bfd2be585
Douglas Teixeira <Doug@MacBook-Pro-3.local>
2017-01-22 12:40:40 -0500

    Add bugfix for captcha in ContactMesController#create.

--- app/controllers/contact_mes_controller.rb
+++ app/controllers/contact_mes_controller.rb
@@ -5,7 +5,7 @@ class ContactMesController < ApplicationController
   end
   
   def create
-  	if (permitted_params.contact_me["terms"])
+  	unless (permitted_params.contact_me["terms"])
 		User.find_by_credentials(ENV["DEV_EMAIL"], ENV["DEV_PASSWORD"]).send_contact_me_email(permitted_params.contact_me)
 	end
 
